3477bbcc73a908c58188b1b2355033a6bc41f504
Sydney Covitz <scovitz1@swarthmore.edu>
2020-10-08 13:56:55 -0400

    rename_files now performs find/replace onall path names in a BIDS directory

--- bond/bond.py
+++ bond/bond.py
@@ -9,15 +9,22 @@ class BOnD(object):
         
     def fieldmaps_ok(self):
         pass
-
-    def rename_files(self, path_to_dir, pattern, replacement):
-        for path in pathlib.Path(path_to_dir).iterdir():
-            if path.is_file():
-                old_name = path.stem 
-                old_ext = path.suffix
-                directory = path.parent
-                new_name = old_name.replace(pattern, replacement) + old_ext
-                path.rename(pathlib.Path(directory, new_name))
+    
+    def rename_files(self, bids_dir, pattern, replacement):
+        # This function performs find/replace on path names in a BIDS directory 
+        # @Params
+        # - bids_dir: a string contianing the path to the bids directory inside which we want to change files 
+        # - pattern: the substring of the file we would like to replace
+        # - replacement: the substring that will replace "pattern"
+        # @Returns
+            # - None 
+        files_and_dirs = Path(bids_dir).rglob('*')
+        for path in files_and_dirs:
+            old_name = path.stem 
+            old_ext = path.suffix
+            directory = path.parent
+            new_name = old_name.replace(pattern, replacement) + old_ext
+            path.rename(pathlib.Path(directory, new_name))
 
     def find_param_sets(self, pattern):
         pass
