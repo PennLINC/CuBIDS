ff54f28673868248d99e1b372b36984995e1d00b
Sydney Covitz <scovitz1@swarthmore.edu>
2020-10-08 12:12:38 -0400

    rename_files function

--- bond/bond.py
+++ bond/bond.py
@@ -1,15 +1,23 @@
 """Main module."""
 
+import pathlib 
+
 class BOnD(object):
 
     def __init__(self, data_root):
         self.path = data_root
-
+        
     def fieldmaps_ok(self):
         pass
 
-    def rename_files(self, pattern, replacement):
-        pass
+    def rename_files(self, path_to_dir, pattern, replacement):
+        for path in pathlib.Path(path_to_dir).iterdir():
+            if path.is_file():
+                old_name = path.stem 
+                old_ext = path.suffix
+                directory = path.parent
+                new_name = old_name.replace(pattern, replacement) + old_ext
+                path.rename(pathlib.Path(directory, new_name))
 
     def find_param_sets(self, pattern):
         pass
