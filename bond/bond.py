c7ed6004fa9a5c156afafc120fcf120d8b2199fe
Matt Cieslak <mattcieslak@gmail.com>
2020-10-09 01:01:50 -0400

    Add data with multiple parameter groups.

--- bond/bond.py
+++ bond/bond.py
@@ -1,6 +1,10 @@
 """Main module."""
 from pathlib import Path
+import bids
+bids.config.set_option('extension_initial_dot', True)
+from bids.util import parse_file_entities
 
+NON_KEY_ENTITIES = set(["subject", "session", "extension"])
 
 class BOnD(object):
 
@@ -11,12 +15,14 @@ class BOnD(object):
         pass
 
     def rename_files(self, pattern, replacement):
+        """
         # @Params
             # - path: a string contianing the path to the bids directory inside which we want to change files
             # - pattern: the substring of the file we would like to replace
             # - replacement: the substring that will replace "pattern"
         # @Returns
             # - None
+        """
         files_and_dirs = Path(self.path).rglob('*')
         for path in files_and_dirs:
             old_name = path.stem
@@ -25,9 +31,55 @@ class BOnD(object):
             new_name = old_name.replace(pattern, replacement) + old_ext
             path.rename(Path(directory, new_name))
 
-    def find_param_sets(self, pattern):
-        pass
+    def get_param_groups(self, key_group):
+        key_entities = _key_group_to_entities(key_group)
+        matching_files = self.layout.get(return_type="file", scope="self",
+                                         **key_entities)
+        return _get_param_groups(matching_files)
+
+    def get_key_groups(self):
+        key_groups = set()
+        for path in Path(self.path).rglob("*.*"):
+            if path.suffix == ".json":
+                continue
+            key_groups.update(_file_to_key_group(path),)
+        return sorted(key_groups)
 
     def fill_metadata(self, pattern, metadata):
         pass
 
+
+def _key_group_to_entities(key_group):
+    return dict([group.split("-") for group in key_group.split("_")])
+
+
+def _entities_to_key_group(entities):
+    group_keys = sorted(entities.keys() - NON_KEY_ENTITIES)
+    return "_".join(
+        ["{}-{}".format(key, entities[key]) for key in group_keys])
+
+
+def _file_to_key_group(filename):
+    entities = parse_file_entities(str(filename))
+    return _entities_to_key_group(entities)
+
+
+def _get_param_groups(files):
+    """Finds a list of *parameter groups* from a list of files.
+
+    Parameters:
+    -----------
+
+    files : list
+        List of file names
+
+    Returns:
+    --------
+
+    parameter_groups : list
+        A list of unique parameter groups
+
+    For each file in `files`, find critical parameters for metadata. Then find
+    unique sets of these critical parameters.
+    """
+    pass
