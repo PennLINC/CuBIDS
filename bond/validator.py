05cd208656eae1c84fc39276ef7df273c0730c4a
TinasheMTapera <tinashemtapera@gmail.com>
2021-04-02 17:18:29 -0400

    begin functionality for running validator sequentially on subjects

--- bond/validator.py
+++ bond/validator.py
@@ -1,6 +1,9 @@
 import subprocess
 import json
 import logging
+import os
+import glob
+import pathlib
 import pandas as pd
 
 logger = logging.getLogger('bond-cli')
@@ -16,17 +19,49 @@ def build_validator_call(path, ignore_headers=False, ignore_subject=True):
         command.append('--ignoreNiftiHeaders')
     if ignore_subject:
         command.append('--ignoreSubjectConsistency')
-
+    
     command.append(path)
 
     return command
 
 
+def build_subject_paths(bids_dir):
+    """Build a list of BIDS dirs with 1 subject each"""
+    
+    if not bids_dir.endswith("/"):
+        bids_dir += "/"
+        
+    root_files = [x for x in glob.glob(bids_dir + "*") if os.path.isfile(x)]
+    
+    bids_dir += "sub-*/"
+    
+    subjects = glob.glob(bids_dir)
+    
+    if len(subjects) < 1:
+        
+        raise ValueError("Couldn't find any subjects "
+                         "in the specified directory:\n" +
+                         bids_dir)
+    
+    subjects_dict = {}
+    
+    for sub in subjects:
+        purepath = pathlib.PurePath(sub)
+        sub_label = purepath.name
+        
+        files = [x for x in glob.glob(sub +'**', recursive=True) if os.path.isfile(x)]
+        files.extend(root_files)
+        subjects_dict[sub_label] = files
+    
+    return subjects_dict
+
+    
 def run_validator(call, verbose=True):
     """Run the validator with subprocess"""
     if verbose:
         logger.info("Running the validator with call:")
         logger.info('\"' + ' '.join(call) + '\"')
+    
     ret = subprocess.run(call, stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
     return(ret)
