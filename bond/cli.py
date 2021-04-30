d1befdfe148973a96c6d7a48b470eef41ec38de8
Sydney Covitz <sydney.covitz@gmail.com>
2021-04-30 14:36:24 -0400

    no more copytree

--- bond/cli.py
+++ bond/cli.py
@@ -139,9 +139,7 @@ def bond_validate():
                         if not os.path.exists(fi_tmpdir):
                             os.makedirs(fi_tmpdir)
                         output = fi_tmpdir + '/' + str(Path(fi).name)
-
-                        # shutil.copy2(fi, output)
-                        shutil.copytree(fi, output)
+                        shutil.copy2(fi, output)
 
                     # run the validator
                     nifti_head = opts.ignore_nifti_headers
