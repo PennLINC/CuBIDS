65558d3fa8c6a56e15e9415f413a8f199ee67db8
TinasheMTapera <tinashemtapera@gmail.com>
2021-05-25 15:58:38 -0400

    new testing branch

--- tests/test_bond.py
+++ tests/test_bond.py
@@ -969,7 +969,7 @@ def test_validator(tmp_path):
 
     parsed = parse_validator_output(ret.stdout.decode('UTF-8'))
 
-    assert parsed.shape[1] > 1
+    assert not 'error' in parsed['severity'].tolist()
 
 
 def test_docker():
