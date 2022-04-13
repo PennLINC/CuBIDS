1d4ac62132f30f9605138c5d83f7505e28defad2
Sydney Covitz <sydney.covitz@gmail.com>
2022-04-13 15:54:16 -0400

    no more github link module

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,6 @@ from packaging import version as pver  # Avoid distutils.LooseVersion which is d
 sys.path.append(os.path.abspath('sphinxext'))
 sys.path.insert(0, os.path.abspath('../wrapper'))
 
-from github_link import make_linkcode_resolve
-
 
 # -- General configuration ---------------------------------------------
 
