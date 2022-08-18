36dec4ed048290ad7d2c904e5ced1bb8d7bd3626
Sydney Covitz <sydney.covitz@gmail.com>
2022-08-18 13:10:00 -0700

    back to original docker run call

--- cubids/cli.py
+++ cubids/cli.py
@@ -232,16 +232,14 @@ def cubids_validate():
     # Run it through a container
     container_type = _get_container_type(opts.container)
     bids_dir_link = str(opts.bids_dir.absolute()) + ":/bids:ro"
-    output_dir_link_t = str(opts.output_prefix.parent.absolute()) + ":/tsv:rw"
-    output_dir_link_j = str(opts.output_prefix.parent.absolute()) + ":/json:rw"
-    linked_output_prefix_t = "/tsv/" + opts.output_prefix.name
-    linked_output_prefix_j = "/json/" + opts.output_prefix.name
+    output_dir_link = str(opts.output_prefix.parent.absolute()) + ":/tsv:rw"
+    linked_output_prefix = "/tsv/" + opts.output_prefix.name
     if container_type == 'docker':
         cmd = ['docker', 'run', '--rm', '-v', bids_dir_link,
                '-v', GIT_CONFIG+":/root/.gitconfig",
-               '-v', output_dir_link_t, output_dir_link_j, '--entrypoint',
+               '-v', output_dir_link, '--entrypoint',
                'cubids-validate', opts.container, '/bids',
-               linked_output_prefix_t, linked_output_prefix_j]
+               linked_output_prefix]
         if opts.ignore_nifti_headers:
             cmd.append('--ignore_nifti_headers')
         if opts.ignore_subject_consistency:
@@ -249,9 +247,8 @@ def cubids_validate():
     elif container_type == 'singularity':
         cmd = ['singularity', 'exec', '--cleanenv',
                '-B', bids_dir_link,
-               '-B', output_dir_link_t, output_dir_link_j, opts.container,
-               'cubids-validate', '/bids', linked_output_prefix_t,
-               linked_output_prefix_j]
+               '-B', output_dir_link, opts.container,
+               'cubids-validate', '/bids', linked_output_prefix]
         if opts.ignore_nifti_headers:
             cmd.append('--ignore_nifti_headers')
         if opts.ignore_subject_consistency:
