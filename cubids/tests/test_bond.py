85d8277039aaffb4ac2e8553de3ab55a13efcbad
Taylor Salo <tsalo006@fiu.edu>
2024-01-12 11:47:05 -0500

    Update the CLIs.

--- cubids/tests/test_bond.py
+++ cubids/tests/test_bond.py
@@ -86,7 +86,7 @@ def test_ok_json_merge_cli(tmp_path):
     )
 
     assert os.path.isfile(source_json)
-    assert not os.path.isfile(dest_json)
+    assert os.path.isfile(dest_json)
     merge_proc = subprocess.run(["bids-sidecar-merge", str(source_json), str(dest_json)])
     assert merge_proc.returncode == 0
     assert not _get_json_string(dest_json) == orig_dest_json_content
