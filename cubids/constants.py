e76304ec072a741c01592622a4fecc99712cb941
Taylor Salo <tsalo006@fiu.edu>
2024-01-22 15:04:29 -0500

    Update constants.py

--- cubids/constants.py
+++ cubids/constants.py
@@ -1,7 +1,13 @@
 """Constants for CuBIDS."""
+# Names of identifier variables.
+# Used to place KeyGroup and ParamGroup at the beginning of a dataframe,
+# but both are hardcoded in the relevant function.
 ID_VARS = set(["KeyGroup", "ParamGroup", "FilePath"])
+# Entities that should not be used to group parameter sets
 NON_KEY_ENTITIES = set(["subject", "session", "extension"])
 # Multi-dimensional keys SliceTiming
+# List of metadata fields and parameters (calculated by CuBIDS)
+# Not sure what this specific list is used for.
 IMAGING_PARAMS = set(
     [
         "ParallelReductionFactorInPlane",
