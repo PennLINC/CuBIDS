bb85c513f099cf74635c01f1b63998d52f69cdd3
Taylor Salo <tsalo006@fiu.edu>
2024-01-31 10:41:54 -0500

    Run isort.

--- cubids/__init__.py
+++ cubids/__init__.py
@@ -1,8 +1,17 @@
 """Top-level package for CuBIDS."""
 
+from cubids import (
+    cli,
+    config,
+    constants,
+    cubids,
+    metadata_merge,
+    utils,
+    validator,
+    workflows,
+)
 from cubids.__about__ import __copyright__, __credits__, __packagename__, __version__
 from cubids.cubids import CuBIDS
-from cubids import cli, config, constants, cubids, metadata_merge, utils, validator, workflows
 
 __all__ = [
     "__copyright__",
