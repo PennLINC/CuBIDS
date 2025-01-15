df9971000eef09f351d203ed6a1a516cc2a948bd
singlesp <sps253@cornell.edu>
2025-01-15 13:58:35 -0500

    add docstring

--- cubids/config.py
+++ cubids/config.py
@@ -7,7 +7,19 @@ from pkg_resources import resource_filename as pkgrf
 
 
 def load_config(config_file):
-    """Load a YAML file containing a configuration for param groups."""
+    """
+    Load a YAML file containing a configuration for param groups.
+
+    Parameters
+    ----------
+    config_file : str or pathlib.Path, optional
+        The path to the configuration file. If None, the default configuration file is used.
+
+    Returns
+    -------
+    dict
+        The configuration loaded from the YAML file.
+    """
     if config_file is None:
         config_file = Path(pkgrf("cubids", "data/config.yml"))
 
