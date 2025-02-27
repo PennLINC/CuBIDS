433690e10ce96ce32ae18fb30bc5a501bb4cea96
Taylor Salo <salot@pennmedicine.upenn.edu>
2025-02-27 15:59:31 -0500

    Add test.

---
+++ cubids/tests/test_file_collections.py
@@ -0,0 +1,34 @@
+"""Test file collection management in CuBIDS."""
+
+import json
+
+from niworkflows.utils.testing import generate_bids_skeleton
+
+from cubids.tests.utils import TEST_DATA
+from cubids.workflows import add_file_collections
+
+
+def test_add_file_collections(tmp_path):
+    """Test adding file collections to a BIDS dataset."""
+    bids_dir = tmp_path / "add_file_collections"
+    dset_yaml = str(TEST_DATA / "skeletons" / "skeleton_file_collection_01.yml")
+    generate_bids_skeleton(str(bids_dir), dset_yaml)
+    add_file_collections(tmp_path)
+
+    f1 = bids_dir / "sub-01" / "func" / "sub-01_task-rest_echo-3_part-phase_bold.json"
+    assert f1.exists()
+    expected = {
+        "EchoTime": 0.45,
+        "EchoTimes": [0.15, 0.15, 0.3, 0.3, 0.45, 0.45],
+        "Parts": ["mag", "phase", "mag", "phase", "mag", "phase"],
+        "FileCollection": [
+            "bids::sub-01/func/sub-01_task-rest_echo-1_part-mag_bold.nii.gz",
+            "bids::sub-01/func/sub-01_task-rest_echo-1_part-phase_bold.nii.gz",
+            "bids::sub-01/func/sub-01_task-rest_echo-2_part-mag_bold.nii.gz",
+            "bids::sub-01/func/sub-01_task-rest_echo-2_part-phase_bold.nii.gz",
+            "bids::sub-01/func/sub-01_task-rest_echo-3_part-mag_bold.nii.gz",
+            "bids::sub-01/func/sub-01_task-rest_echo-3_part-phase_bold.nii.gz",
+        ],
+        "Units": "arbitrary",
+    }
+    assert json.loads(f1.read_text()) == expected
